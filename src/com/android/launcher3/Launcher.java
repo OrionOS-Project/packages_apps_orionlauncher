527b32cd0e6be76e349f3e9f56a3426c955f113b
Wrdn28 <ifloraxd@gmail.com>
2025-05-14 03:13:47 +0000

    Revert "Launcher3: release memory whenever possible"
    This reverts commit 5dea47d3d2757267b8a736169fb97931a610ae08.

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1357,15 +1357,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         TraceHelper.INSTANCE.endSection();
 
         LauncherAppState.INSTANCE.executeIfCreated(app -> app.checkIfRestartNeeded());
-        
-        releaseMemory();
-    }
-
-    private void releaseMemory() {
-        try {
-            android.app.ActivityManager.getService().releaseMemory(900, 20, false, false);
-        } catch (Exception e) {
-        }
     }
 
     @Override
@@ -1387,8 +1378,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         mAppWidgetHolder.setActivityResumed(false);
         
         mHandler.postDelayed(mLauncherGcOpt, 1000);
-        
-        releaseMemory();
     }
 
     /**
